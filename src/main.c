973c46bf3da6fd4b0202a21ae8c6cc7e2acac166
Rafael Moura <rafaelmoura.dev@gmail.com>
2017-12-05 17:56:26 -0200

    added freq control

--- src/main.c
+++ src/main.c
@@ -8,15 +8,21 @@
 noreturn void main(void)
 {
 
-	const int freq = 1;
+	int freq = 1;
 	DDRD |= 0x01<<PD7;
+	DDRD &= ~(0x01<<PD6);
 	PORTD &= ~(0x01<<PD7);
+	PORTD |= 0x01<<PD6;
 
 	for (;;) {
 		PORTD |= 0x01<<PD7;
 		_delay_ms(1000 / freq);
 		PORTD &= ~(0x01<<PD7);
 		_delay_ms(1000 / freq);
+		if (!(PIND&(0x01<<PD6))) {
+			freq += 1;
+			_delay_ms(1000);
+		}
 	}
 
 }
